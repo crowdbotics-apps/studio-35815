5de635cf36bd5fd4030a829acc2d2fe5a0fefa9b
Mithilesh@crowdbotics.com <Mithilesh@crowdbotics.com>
2022-12-15 12:04:38 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,23 +1,29 @@
+import { StyleSheet } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
 const Untitled1 = () => {
-  return <View style={{
-    backgroundColor: '#f0f0f1',
-    padding: 10,
-    position: 'relative',
-    height: '100%'
-  }}>
-      <View style={{
-      left: 41,
-      top: 161,
-      position: "absolute",
-      height: 60,
-      width: 140,
-      backgroundColor: "#E4E4E4",
-      borderRadius: 0,
-      color: "#777777"
-    }}></View></View>;
+  return <View style={_styles.PEXQsRdN}>
+      </View>;
 };
 
 export default Untitled1;
+
+const _styles = StyleSheet.create({
+  PEXQsRdN: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  GEYZbEcU: {
+    left: 41,
+    top: 161,
+    position: "absolute",
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  }
+});
--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,24 +1,29 @@
-import { Text } from "react-native";
+import { StyleSheet } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
 const Untitled2 = () => {
-  return <View style={{
-    backgroundColor: '#f0f0f1',
-    padding: 10,
-    position: 'relative',
-    height: '100%'
-  }}>
-      <Text style={{
-      left: 90,
-      top: 69,
-      position: "absolute",
-      width: 100,
-      height: 50,
-      lineHeight: 14,
-      fontSize: 14,
-      borderRadius: 0
-    }}>Lorem ipsum… </Text></View>;
+  return <View style={_styles.tDYVUyrH}>
+      </View>;
 };
 
 export default Untitled2;
+
+const _styles = StyleSheet.create({
+  tDYVUyrH: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  UFkwamwr: {
+    left: 90,
+    top: 69,
+    position: "absolute",
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  }
+});
